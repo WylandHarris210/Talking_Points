43d692a06e9dc11df31acde6f4de89ad304a2095
Wyland Harris <wyland.harris@gmail.com>
2023-02-25 13:22:42 -0600

    URL's

--- parse/parse.js
+++ parse/parse.js
@@ -1,6 +1,9 @@
 const data = require('./dev.json')[0].trends;
 
+
+
 console.log("All trending topics are:");
 for(let i = 0; i < data.length; i++) {
-    console.log(data[i].name);
+    console.log("http://twitter.com/search?q=" + data[i].query);
 }
+

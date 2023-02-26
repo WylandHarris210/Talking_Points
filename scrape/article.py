df943d642ebd36932f618b40ec3ebfd77a81a248
Wyland Harris <wyland.harris@gmail.com>
2023-02-25 21:19:27 -0600

    All links to json file

--- scrape/article.py
+++ scrape/article.py
@@ -2,22 +2,24 @@ import io
 import pandas as pd
 import requests as r
 from bs4 import BeautifulSoup
+import json
 
 def main():
     url = 'http://news.google.com/search?q='
-    header = {'user_agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'}
+    # header = {'user_agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'}
 
-    # search = input('What would you like to look for?\n')
+    search = input('What would you like to look for?\n')
 
-    res = r.get(url + 'Tennis', headers = header)
-    # res = r.get(url + search)
+    # res = r.get(url + 'Tennis', headers = header)
+    res = r.get(url + search)
+    # res = r.get(url + search, headers = header)
 
     if res.status_code != 200:
         print('Bad request, recieved code', res.status_code)
         exit()
 
     print('Request is good')
-    # print(res.)
+    
     soup = BeautifulSoup(res.content, 'lxml')
 
     stories = soup.find('div', attrs = { 'class': 'lBwEZb BL5WZb GndZbb' })
@@ -29,15 +31,10 @@ def main():
     for i in range(3):
         stories_urls[i] = 'https://news.google.com' + stories_list[i].find('a', attrs = { 'class': 'VDXfz' })['href'][1:]
 
-    article1 = getChildArticle(stories_urls[0])
-    article2 = getChildArticle(stories_urls[1])
-    article3 = getChildArticle(stories_urls[2])
+    stories_urls_json = json.dumps(stories_urls)
 
-
-
-getChildArticle():
-    print("Article")
-
+    with open('scrape/links.json', 'w') as outfile:
+        outfile.write(stories_urls_json)
 
 if __name__ == '__main__':
     main()

b7e9d809ae341e9618c1a6ce3871f3f121c2e0c2
xanswine <100490925+xanswine@users.noreply.github.com>
2023-02-25 22:27:01 -0600

    a

--- scrape/article.py
+++ scrape/article.py
@@ -8,7 +8,7 @@ def main():
     url = 'http://news.google.com/search?q='
     # header = {'user_agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'}
 
-    search = input('What would you like to look for?\n')
+    search  = input('What would you like to look for?\n')
 
     # res = r.get(url + 'Tennis', headers = header)
     res = r.get(url + search)

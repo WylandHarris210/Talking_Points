c05870d92f04d874fde2613731f384b271fc7284
Ben Hobbs <benhobbs2022@gmail.com>
2023-02-25 21:58:41 -0600

    Updated key

--- parse/parse.js
+++ parse/parse.js
@@ -3,7 +3,7 @@ const data = require('./dev.json')[0].trends;
 const topThreeResults = [];
 
 
-for(let i = 0; i < 3; i++) {
+for(let i = 0; i < 25; i++) {
   // Internet Link to Tweetconsole.log("http://twitter.com/search?q=" + data[i].query);
   topThreeResults[i] = data[i].name;
 }
@@ -11,9 +11,9 @@ for(let i = 0; i < 3; i++) {
 console.log("All trending topics are:");
 console.log(topThreeResults);
 
-const topResult = topThreeResults[1];
+const topResult = topThreeResults[0];
 
-const apiKey = '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';
+const apiKey = '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';
 const prompt = "'Tell me about '";
 
 function getAnswerFromOpenAI2(apiKey2, prompt2, topResult){

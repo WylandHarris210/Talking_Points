96c5803e692ec33726e3baccd0726cae514c5532
WylandHarris210 <73365364+WylandHarris210@users.noreply.github.com>
2023-03-07 12:32:16 -0600

    Push

---
+++ ScrapenTweeter/Tweetercheck.py
@@ -0,0 +1,68 @@
+import pyttsx3
+import json
+import requests
+import pandas as pd
+import csv
+
+def twireq():
+    url = "https://api.twitter.com/1.1/trends/place.json?id=23424977"
+    payload={}
+    headers = {
+    'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAALCVbAEAAAAAM3chpDW3NCSGAjk7%2FwCa%2B%2BbWWRk%3DN9dO47Lx9L9tEJXcaBWgehlZDuaVCGyl1ficelzA0EZrjY2uNA',
+    'Cookie': 'guest_id=v1%3A167734669699093551; guest_id_ads=v1%3A167734669699093551; guest_id_marketing=v1%3A167734669699093551; personalization_id="v1_zXS78KySxicfVHRnP7YLWw=="'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    r = response.json()
+    with open('parse/dev.json', 'w') as f:
+        json.dump(r, f)
+
+    topics_list = r[0]['trends']
+    good_topics = []
+
+    for topic in topics_list:
+        if not ('#' in topic['name']):
+            good_topics.append(topic['name'])
+            if len(good_topics) == 5:
+                break
+
+    with open('scrape/topics.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(good_topics)
+
+    temp = 1
+        
+
+
+globarr = {}
+def get_element_from_json(json_str, key):
+    json_obj = json.loads(json_str)
+    return json_obj[key]
+def readjson():
+    # with open("dev.json") as file:
+    #     js = json.load(file)
+    #     x = len(js['trends'])
+    #     for i in range(0,x):
+    #         globarr.append(js['trend'][0]["name"])
+    data = json.loads('dev.json')
+    names = [trend["name"] for trend in data[0]["trends"]]
+    print(names)
+def get_query(json_str):
+    data = json.loads(json_str)
+    queries = [trends["query"] for trends in data[0]["trends"]]
+    return queries
+
+def ttsorg():
+    x='temp'
+    while(x!=''):
+        x = input()
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id) 
+        engine.setProperty('rate', 111) 
+        engine.say(x)
+        engine.runAndWait()
+# readjson()
+# print(globarr)
+# ttsorg()
+# get_query('dev.json')
+twireq()
--- scrape/article.py
+++ scrape/article.py
@@ -10,8 +10,6 @@ NUM_TOPICS = 5
 
 def getLinks(topic, num):
     url = 'http://news.google.com/search?q='
-    # header = {'user_agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'}
-
     res = r.get(url + topic)
 
     if res.status_code != 200:
@@ -21,21 +19,13 @@ def getLinks(topic, num):
     print('Request is good')
     
     soup = BeautifulSoup(res.content, 'lxml')
-
     stories = soup.find('div', attrs = { 'class': 'lBwEZb BL5WZb GndZbb' })
-
     stories_list = stories.find_all('div', attrs = { 'class': 'NiLAwe y6IFtc R7GTQ keNKEd j7vNaf nID9nc' })[:3]
-
     stories_urls = [None] * NUM_ARTICLES
-
     for i in range(NUM_ARTICLES):
         stories_urls[i] = 'https://news.google.com' + stories_list[i].find('a', attrs = { 'class': 'VDXfz' })['href'][1:]
 
-    # stories_urls_json = json.dumps(stories_urls)
     return stories_urls
-
-    # with open('scrape/links' + str(num) + '.json', 'w') as outfile:
-    #     outfile.write(stories_urls_json)
 
 def main():
     big_chungus = dict()
@@ -46,6 +36,7 @@ def main():
             # big_chungus[str(reader[0][i])] = getLinks(reader[0][i], i)
 
     with open('parse/outputs.json', 'w') as outfile:
-        # outfile.write(json.dumps(big_chungus))
-        temp = json.dumps(big_chungus)
-        outfile.write(temp)
+        outfile.write(json.dumps(big_chungus))
+
+if __name__ == '__main__':
+    main()
